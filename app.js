bf93546d0c677e9fdb7c9d90a9df2fb5b079eed7
김현우 <hyunwoo.kim@wavle.io>
2022-04-06 13:34:39 +0900

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post("/webhook", (req, res, next) => {
+  console.log(req.body);
+  res.json({ code: 0 });
+});
+
+app.listen(app.get("port"), () => {
+  console.log("Listen at ", app.get("port"));
+});
