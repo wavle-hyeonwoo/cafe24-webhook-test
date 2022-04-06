9600f6271461a835f34b7bee0379e683a80f2039
김현우 <hyunwoo.kim@wavle.io>
2022-04-06 17:34:17 +0900

    oauth

--- a.js
+++ a.js
@@ -1,20 +1,33 @@
+const fetch = require("node-fetch");
 const qs = require("querystring");
+const { URLSearchParams } = require("url");
+const { default: axios } = require("axios");
 
-// console.log(
-//   qs.encode({
-//     response_type: "code",
-//     client_id: process.env.CAFE24_CLIENT_ID,
-//     state: `asdfadsfasdfasdfasdfadsf`,
-//     redirect_uri: `https://cafe24-webhook-test.herokuapp.com/cafe24/authcode`,
-//     scope: `mall.read_application`,
-//   })
-// );
+const run = async (authcode) => {
+  const url = `http://localhost:3001`;
+  axios
+    .post(
+      url,
+      qs.encode({
+        code: authcode,
+        grant_type: "authorization_code",
+        redirect_uri:
+          "https://cafe24-webhook-test.herokuapp.com/cafe24/accesstoken",
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Basic ${Buffer.from(
+            process.env.CAFE24_CLIENT_ID +
+              ":" +
+              process.env.CAFE24_CLIENT_SECRET
+          ).toString("base64")}`,
+        },
+      }
+    )
+    .catch((e) => {
+      console.error("ERROR get-token", e);
+    });
+};
 
-console.log(
-  qs.encode({
-    code: "1234",
-    grant_type: "authorization_code",
-    redirect_uri:
-      "https://cafe24-webhook-test.herokuapp.com/cafe24/accesstoken",
-  })
-);
+run("abcd");
