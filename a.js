0db39799bae88ff819a10b42ee5507ac23bac7d7
김현우 <hyunwoo.kim@wavle.io>
2022-04-06 16:28:31 +0900

    oauth authcode (fix)

--- a.js
+++ a.js
@@ -1,11 +1,17 @@
 const qs = require("querystring");
 
+// console.log(
+//   qs.encode({
+//     response_type: "code",
+//     client_id: process.env.CAFE24_CLIENT_ID,
+//     state: `asdfadsfasdfasdfasdfadsf`,
+//     redirect_uri: `https://cafe24-webhook-test.herokuapp.com/cafe24/authcode`,
+//     scope: `mall.read_application`,
+//   })
+// );
+
 console.log(
-  qs.encode({
-    response_type: "code",
-    client_id: process.env.CAFE24_CLIENT_ID,
-    state: `asdfadsfasdfasdfasdfadsf`,
-    redirect_uri: `https://cafe24-webhook-test.herokuapp.com/cafe24/authcode`,
-    scope: `mall.read_application`,
+  qs.stringify({
+    grant_type: "authorization_code",
   })
 );
