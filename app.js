4797df98d738f230303b65f7346a55c77b49ce69
김현우 <hyunwoo.kim@wavle.io>
2022-04-06 15:04:01 +0900

    oauth authcode

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 
+const getAuthcode = require("./lib/get-authcode");
+
 app.set("port", process.env.PORT || 3000);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post("/webhook", (req, res, next) => {
   console.log("[HOOK]\n", req.body, "\n\n\n");
+  getAuthcode();
+  res.json({ code: 0 });
+});
+
+app.get("/cafe24/authcode", (req, res, next) => {
+  console.log("QUERY: ", req.query);
   res.json({ code: 0 });
 });
 
