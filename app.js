02cab729a9f7bb9acf8b10cd4ed6e697db07e256
김현우 <hyunwoo.kim@wavle.io>
2022-04-08 12:50:08 +0900

    cafe24 api call test

--- app.js
+++ app.js
@@ -23,33 +23,38 @@ app.get("/", async (req, res, next) => {
     const response = await axios.get(baseUrl + query, config);
     res.json(response.data);
   } catch (e) {
-    console.error("ERROR Occured. It seems that a access token has expired.");
-    const refreshResult = await axios.post(
-      `https://clubpetworld.cafe24api.com/api/v2/oauth/token`,
-      qs.stringify({
-        grant_type: "refresh_token",
-        refresh_token: "XqmIe08ICSWVFxTzjIIZfP",
-      }),
-      {
-        headers: {
-          Authorization: `Basic ${Buffer.from(
-            process.env.CAFE24_CLIENT_ID +
-              ":" +
-              process.env.CAFE24_CLIENT_SECRET
-          ).toString("base64")}`,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
-
-    config.headers[
-      "Authorization"
-    ] = `Bearer ${refreshResult.data.access_token}`;
-
-    const response = await axios.get(baseUrl + query, config);
-    console.log("#1");
-    res.json(response.data);
+    res.json({
+      result: "ERROR",
+      error: e,
+    });
   }
+  //   console.error("ERROR Occured. It seems that a access token has expired.");
+  //   const refreshResult = await axios.post(
+  //     `https://clubpetworld.cafe24api.com/api/v2/oauth/token`,
+  //     qs.stringify({
+  //       grant_type: "refresh_token",
+  //       refresh_token: "XqmIe08ICSWVFxTzjIIZfP",
+  //     }),
+  //     {
+  //       headers: {
+  //         Authorization: `Basic ${Buffer.from(
+  //           process.env.CAFE24_CLIENT_ID +
+  //             ":" +
+  //             process.env.CAFE24_CLIENT_SECRET
+  //         ).toString("base64")}`,
+  //         "Content-Type": "application/x-www-form-urlencoded",
+  //       },
+  //     }
+  //   );
+
+  //   config.headers[
+  //     "Authorization"
+  //   ] = `Bearer ${refreshResult.data.access_token}`;
+
+  //   const response = await axios.get(baseUrl + query, config);
+  //   console.log("#1");
+  //   res.json(response.data);
+  // }
 });
 
 app.post("/webhook", (req, res, next) => {
